a7f3a0975813265274ab92bd41cbedd47607209f
tuttdev98 <tuttdev98@gmail.com>
2022-02-28 18:34:02 +0700

    U

--- script.js
+++ script.js
@@ -1,65 +1,136 @@
-let videoContainer = document.querySelector('#videos');
+const videoContainer = document.querySelector("#videos");
 
 const vm = new Vue({
     el: "#app",
     data: {
-        userToken: '',
-        roomToken: '',
-        roomId: '',
+        userToken: "",
+        roomId: "",
+        roomToken: "",
         room: undefined,
-        client: undefined,
+        callClient: undefined
     },
-    mounted() {
+    computed: {
+        roomUrl: function() {
+            return `https://${location.hostname}?room=${this.roomId}`;
+        }
+    },
+    async mounted() {
         api.setRestToken();
+
+        const urlParams = new URLSearchParams(location.search);
+        const roomId = urlParams.get("room");
+        if (roomId) {
+            this.roomId = roomId;
+
+            await this.join();
+        }
     },
     methods: {
-
-        login: function() {
+        authen: function() {
             return new Promise(async resolve => {
-                const userId = (Math.random() * 10000).toFixed(0);
+                const userId = `${(Math.random() * 100000).toFixed(6)}`;
                 const userToken = await api.getUserToken(userId);
                 this.userToken = userToken;
-                const client = new StringeeClient();
-                client.on("authen", (result) => {
-                    console.log(result);
-                    resolve(result);
-                });
-                client.connect(userToken);
-                this.client = client;
-            })
-        },
-        publishVideo: async function() {
 
-            const localTrack = await StringeeVideo.createLocalVideoTrack(this.client, {
-                audio: true,
-                video: true,
-                videoDimensions: { width: 640, height: 360 }
+                if (!this.callClient) {
+                    const client = new StringeeClient();
+
+                    client.on("authen", function(res) {
+                        console.log("on authen: ", res);
+                        resolve(res);
+                    });
+                    this.callClient = client;
+                }
+                this.callClient.connect(userToken);
             });
+        },
+        publish: async function(screenSharing = false) {
+            const localTrack = await StringeeVideo.createLocalVideoTrack(
+                this.callClient, {
+                    audio: true,
+                    video: true,
+                    screen: screenSharing,
+                    videoDimensions: { width: 640, height: 360 }
+                }
+            );
 
-            var videoElement = localTrack.attach();
-            videoContainer.appendChild(videoElement);
+            const videoElement = localTrack.attach();
+            this.addVideo(videoElement);
+
+            const roomData = await StringeeVideo.joinRoom(
+                this.callClient,
+                this.roomToken
+            );
+            const room = roomData.room;
+            console.log({ roomData, room });
+
+            if (!this.room) {
+                this.room = room;
+                room.clearAllOnMethos();
+                room.on("addtrack", e => {
+                    const track = e.info.track;
+
+                    console.log("addtrack", track);
+                    if (track.serverId === localTrack.serverId) {
+                        console.log("local");
+                        return;
+                    }
+                    this.subscribe(track);
+                });
+                room.on("removetrack", e => {
+                    const track = e.track;
+                    if (!track) {
+                        return;
+                    }
+
+                    const mediaElements = track.detach();
+                    mediaElements.forEach(element => element.remove());
+                });
+
+                // Join existing tracks
+                roomData.listTracksInfo.forEach(info => this.subscribe(info));
+            }
+
+            await room.publish(localTrack);
+            console.log("room publish successful");
         },
         createRoom: async function() {
             const room = await api.createRoom();
-            const roomToken = await api.getRoomToken(room.roomId);
-
-            this.roomId = room.roomId;
-            this.roomToken = roomToken;
-
-            await this.login();
-            await this.publishVideo();
-        },
-
-        joinRoom: async function() {
-            const roomId = prompt('Dán Room ID vào Đây');
-            if (!roomId) {
-                return
-            }
-
+            const { roomId } = room;
             const roomToken = await api.getRoomToken(roomId);
+
             this.roomId = roomId;
             this.roomToken = roomToken;
+            console.log({ roomId, roomToken });
+
+            await this.authen();
+            await this.publish();
+        },
+        join: async function() {
+            const roomToken = await api.getRoomToken(this.roomId);
+            this.roomToken = roomToken;
+
+            await this.authen();
+            await this.publish();
+        },
+        joinWithId: async function() {
+            const roomId = prompt("Dán Room ID vào đây nhé!");
+            if (roomId) {
+                this.roomId = roomId;
+                await this.join();
+            }
+        },
+        subscribe: async function(trackInfo) {
+            const track = await this.room.subscribe(trackInfo.serverId);
+            track.on("ready", () => {
+                const videoElement = track.attach();
+                this.addVideo(videoElement);
+            });
+        },
+        addVideo: function(video) {
+            video.setAttribute("controls", "true");
+            video.setAttribute("playsinline", "true");
+            videoContainer.appendChild(video);
         }
     }
-
 });
